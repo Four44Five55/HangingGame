6f0510ec63ceb72a13180aa73ae52558f4b174d0
Four44Five55 <Four4Five55@yandex.ru>
2024-12-03 19:44:11 +0300

    -добавлен класс отвечающий за вывод в консоль элементов фигуры в зависимости от вх.параметра по количеству ошибок

---
+++ src/main/java/org/example/PrintHanging.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class PrintHanging {
+    char[][] body = new char[3][3];
+    char[] skeleton = new char[6];
+
+    public PrintHanging() {
+        for (char[] row : body) {
+            Arrays.fill(row, ' ');
+        }
+        skeleton[0] = 'O';
+        skeleton[1] = '/';
+        skeleton[2] = '|';
+        skeleton[3] = '\\';
+        skeleton[4] = '/';
+        skeleton[5] = '\\';
+    }
+
+    void printMan(int counterMistakes) {
+        for (int i = 0; i <= 5; i++) {
+
+            if (i >= counterMistakes) {
+                skeleton[i] = ' ';
+            }
+        }
+
+        body[0][1] = skeleton[0];
+        body[1][0] = skeleton[1];
+        body[1][1] = skeleton[2];
+        body[1][2] = skeleton[3];
+        body[2][0] = skeleton[4];
+        body[2][2] = skeleton[5];
+
+        for (int indRow = 0; indRow < 3; indRow++) {
+            for (int indCol = 0; indCol < 3; indCol++) {
+                System.out.print(body[indRow][indCol]);
+            }
+            System.out.println();
+        }
+    }
+
+
+}
